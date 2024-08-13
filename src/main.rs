811e8ff91b92d40b967503d4fe99e2c0cd78b31a
eeeyrie <eeeyrie@proton.me>
2024-08-13 14:48:45 -0400

    asdffdas

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
             if pattern.chars().count() == 1 {
                 return input_line.contains(pattern);
             } else if pattern.starts_with('[') && pattern.ends_with(']') {
-                println!("pattern matched");
+                println!("{}", &pattern.trim_start_matches('[').trim_end_matches(']')[..]));
                 return input_line.contains(&pattern.trim_start_matches('[').trim_end_matches(']')[..])
             }
 
